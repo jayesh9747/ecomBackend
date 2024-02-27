cd388a9be78dfbf5a4723d0a0f8005b4feb55ba3
Jayesh Savaliya <jayeshsavaliya009@gmail.com>
2024-02-27 16:11:03 +0530

    adding cors module

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const bodyParser = require("body-parser");
 const express = require('express');
+const cors = require("cors");
 
-
-const {connectMongoDB} = require('./connection')
+const { connectMongoDB } = require('./connection')
 const auth = require('./routes/auth')
 const categories = require("./routes/categories");
 const user = require('./routes/users')
@@ -15,20 +15,21 @@ const PORT = process.env.PORT || 8001;
 
 
 //middleware 
+app.use(cors());
+app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 //routes
-app.use('/api/auth',auth);
+app.use('/api/auth', auth);
 app.use("/api/categories", categories);
-app.use('/api/user',user);
+app.use('/api/user', user);
 app.use("/api/listings", listings);
 app.use("/api/my", my);
 app.use("/api/messages", messages);
 
 
-app.get('/',async(req,res)=>{
-    res.json({msg : "Hello world"})
+app.get('/', async (req, res) => {
+    res.json({ msg: "Hello world" })
 })
 
 
@@ -40,6 +41,6 @@ connectMongoDB(process.env.MONGO_URI)
     })
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`server is running on port: ${PORT}`);
 })
